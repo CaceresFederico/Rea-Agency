502676976093dc1bc8df6832259a83d890b9c6a7
Federico <Caceresfederico@hotmail.com>
2023-09-26 00:20:56 -0300

    About-Contact-Footer Section added with support for: PC and tablet Responsive Design

---
+++ main.js
@@ -0,0 +1,21 @@
+// THIS RETURNING HTML COLECTION
+const profiles = document.getElementsByClassName("profile-thumbnail");
+const tooltips = document.getElementsByClassName("tooltip");
+// CONVERT TO ARRAY TO WORK WITH THE DATA WITH SPREAD OPERATOR
+const tooltipArray = [...tooltips];
+const profilesArray = [...profiles];
+
+// SHOWING TOOLTIPS MOUSEOVER AND MOUSEOUT
+// TO FIX SHOW INDIVIDUAL TOOLTIPS
+profilesArray.forEach((profile) => {
+  profile.addEventListener("mouseover", function () {
+    tooltipArray.forEach((item) => {
+      item.classList.toggle("hidden");
+    });
+  });
+  profile.addEventListener("mouseout", () => {
+    tooltipArray.forEach((item) => {
+      item.classList.toggle("hidden");
+    });
+  });
+});
